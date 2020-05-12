030aae6549158d12289116df9c3840f7211c6909
Alex Oliynyk <asashayko@gmail.com>
2020-05-12 18:27:35 +0800

    add frame progress bar from 4 different corners (clockwise only)

--- example/src/App.js
+++ example/src/App.js
@@ -4,7 +4,14 @@ import { VideoProgress } from 'react-video-progress'
 import 'react-video-progress/dist/index.css'
 
 const App = () => {
-  return <VideoProgress/>
+  const containerRef = React.useRef(null)
+  return <VideoProgress
+  ref={containerRef}
+  pathColor="blue"
+  pathWidth="10px"
+  controls
+  src='https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4'
+  />
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,83 +1,158 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+// import styles from './styles.module.css'
 
-export const VideoProgress = () => {
-  const videoRef = React.useRef<HTMLVideoElement>(null)
+enum StartOptions {
+  BottomLeft = 'BottomLeft',
+  TopLeft = 'TopLeft',
+  TopRight = 'TopRight',
+  BottomRight = 'BottomRight'
+}
+
+interface VideoProps extends React.HTMLProps<HTMLVideoElement> {
+  pathColor?: string
+  pathWidth?: string
+  progressStart?: StartOptions
+  onLoadedMetadata?(e: React.SyntheticEvent<HTMLVideoElement>): void
+  onTimeUpdate?(e: React.SyntheticEvent<HTMLVideoElement>): void
+}
+
+function getLengthes({
+  path,
+  width,
+  height,
+  progressStart
+}: {
+  path: number
+  width: number
+  height: number
+  progressStart: StartOptions
+}) {
+  let top = 0
+  let right = 0
+  let bottom = 0
+  let left = 0
+  switch (progressStart) {
+    case StartOptions.BottomLeft:
+      left = path > height ? height : path
+      top = path > height + width ? width : path - height
+      right = path > height * 2 + width ? height : path - height - width
+      bottom = path > height * 2 + width * 2 ? width : path - height * 2 - width
+      break
+    case StartOptions.TopLeft:
+      top = path > width ? width : path
+      right = path > height + width ? height : path - width
+      bottom = path > width * 2 + height ? width : path - height - width
+      left = path > height * 2 + width * 2 ? height : path - height * 2 - width
+      break
+    case StartOptions.TopRight:
+      right = path > height ? height : path
+      bottom = path > height + width ? width : path - height
+      left = path > height * 2 + width ? height : path - height - width
+      top = path > height * 2 + width * 2 ? width : path - height * 2 - width
+      break
+    case StartOptions.BottomRight:
+      bottom = path > width ? width : path
+      left = path > height + width ? height : path - width
+      top = path > width * 2 + height ? width : path - height - width
+      right = path > height * 2 + width * 2 ? height : path - height * 2 - width
+      break
+    default:
+      break
+  }
+  return {
+    top,
+    right,
+    bottom,
+    left
+  }
+}
+
+export const VideoProgress = ({
+  pathColor = 'red',
+  pathWidth = '5px',
+  progressStart = StartOptions.TopLeft,
+  onLoadedMetadata = () => {},
+  onTimeUpdate = () => {},
+  ...videoProps
+}: VideoProps) => {
   const containerRef = React.useRef<HTMLDivElement>(null)
   const [duration, setDuration] = React.useState(0)
   const [currentTime, setCurrentTime] = React.useState(0)
 
-  const dimensions = containerRef?.current?.getBoundingClientRect()
-  const totalLength = dimensions
-    ? (dimensions?.width + dimensions?.height) * 2
-    : 0
+  const { width, height } = containerRef?.current?.getBoundingClientRect() ?? {
+    width: 0,
+    height: 0
+  }
+  const totalLength = (width + height) * 2
   const step = totalLength / duration
   const path = currentTime * step
 
-  const top = dimensions && path > dimensions?.width ? dimensions?.width : path
-  const right =
-    dimensions && path > dimensions?.width + dimensions?.height
-      ? dimensions?.height
-      : dimensions?.width
-      ? path - dimensions?.width
-      : 0
-  const bottom =
-    dimensions && path > dimensions?.width * 2 + dimensions?.height
-      ? dimensions?.width
-      : dimensions?.width && dimensions?.height
-      ? path - dimensions?.width - dimensions?.height
-      : 0
-  const left =
-    dimensions && path > dimensions?.width * 2 + dimensions?.height * 2
-      ? dimensions?.height
-      : dimensions?.width && dimensions?.height
-      ? path - dimensions?.width * 2 - dimensions?.height
-      : 0
+  const { top, right, bottom, left } = getLengthes({
+    path,
+    width,
+    height,
+    progressStart
+  })
 
-  console.log('dur', step, path, currentTime, duration)
+  const commonStyles: React.CSSProperties = {
+    position: 'absolute',
+    zIndex: 2,
+    backgroundColor: pathColor
+  }
 
   return (
-    <div className={styles.videoContainer} ref={containerRef}>
+    <div
+      style={{
+        display: 'inline-block',
+        position: 'relative',
+        margin: '20px'
+      }}
+      ref={containerRef}
+    >
       <div
-        className={styles.top}
         style={{
+          ...commonStyles,
           width: `${top}px`,
-          height: '5px'
+          height: `${pathWidth}`,
+          top: 0
         }}
       />
       <div
-        className={styles.right}
         style={{
-          width: `5px`,
-          height: `${right}px`
+          ...commonStyles,
+          width: `${pathWidth}`,
+          height: `${right}px`,
+          right: 0
         }}
       />
       <div
-        className={styles.bottom}
         style={{
+          ...commonStyles,
           width: `${bottom}px`,
-          height: '5px'
+          height: `${pathWidth}`,
+          bottom: 0,
+          right: 0
         }}
       />
       <div
-        className={styles.left}
         style={{
-          width: `5px`,
-          height: `${left}px`
+          ...commonStyles,
+          width: `${pathWidth}`,
+          height: `${left}px`,
+          left: 0,
+          bottom: 0
         }}
       />
       <video
-        ref={videoRef}
-        style={{ zIndex: 0, position: 'relative' }}
         onLoadedMetadata={(e: React.SyntheticEvent<HTMLVideoElement>) => {
           setDuration(e.currentTarget.duration)
+          onLoadedMetadata(e)
         }}
         onTimeUpdate={(e: React.SyntheticEvent<HTMLVideoElement>) => {
-          // console.log('play', e.currentTarget.currentTime)
           setCurrentTime(e.currentTarget.currentTime)
+          onTimeUpdate(e)
         }}
-        controls
-        src='https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4'
+        {...videoProps}
       />
     </div>
   )
